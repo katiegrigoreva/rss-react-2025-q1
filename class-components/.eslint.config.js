8549c879e9c4d12d0f3866d8aa0455b08fcb4874
Kate <e.khizova@gmail.com>
2024-07-04 14:19:57 +0300

    chore: add dependencies and configs for react, vite, typescript, eslint, prettier

---
+++ class-components/.eslint.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2021: true },
+  extends: [
+    'prettier',
+    'airbnb-base',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended'
+  ],
+  ignorePatterns: ['dist', '.eslintrc.config.js'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh', 'prettier', '@typescript-eslint'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+  },
+}
