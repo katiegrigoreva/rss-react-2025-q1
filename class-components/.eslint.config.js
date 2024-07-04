a93a84afa1eaeb3ac5425ecb51e8664a99304ff1
Kate <e.khizova@gmail.com>
2024-07-04 19:10:27 +0300

    chore: add changes to configuration

--- class-components/.eslint.config.js
+++ class-components/.eslint.config.js
@@ -7,15 +7,12 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
-    'plugin:prettier/recommended'
+    'plugin:prettier/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.config.js'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh', 'prettier', '@typescript-eslint'],
+  plugins: ['react-refresh', 'prettier', '@typescript-eslint', 'import'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
   },
-}
+};
