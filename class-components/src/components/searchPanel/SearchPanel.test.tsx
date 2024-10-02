db883062c96d65e80347368cb8bfb6694ac8fa00
Kate <e.khizova@gmail.com>
2024-10-02 12:05:04 +0300

    feat: add tests for search panel component

---
+++ class-components/src/components/searchPanel/SearchPanel.test.tsx
@@ -0,0 +1,53 @@
+import { beforeEach } from 'node:test';
+import { describe, expect, it } from 'vitest';
+import { fireEvent, render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from '../../store';
+import Main from '../pages/mainPage/MainPage';
+
+const localStorageMock = (function () {
+  const localStorageStore: { [key: string]: string } = {};
+
+  const setItem = (key: string, value: string) => {
+    Object.defineProperty(store, key, value);
+  };
+  const getItem = (key: string): string => {
+    return localStorageStore[key];
+  };
+  const clear = () => {
+    delete localStorageStore.key;
+  };
+  return { setItem, getItem, clear };
+})();
+
+Object.defineProperty(window, 'localStorage', localStorageMock);
+
+describe('Test searchPanel', () => {
+  beforeEach(() => {
+    localStorage.clear();
+  });
+  it('saves the entered value to the local storage', () => {
+    render(
+      <BrowserRouter>
+        <Provider store={store}>
+          <Main />
+        </Provider>
+      </BrowserRouter>
+    );
+    const input = screen.getByPlaceholderText(/Search/i);
+    fireEvent.change(input, { target: { value: 'mockValue' } });
+    expect(localStorage.getItem('searchTerm')).toBe('mockvalue');
+  });
+  it('retrieves the value from the local storage upon mounting', () => {
+    render(
+      <BrowserRouter>
+        <Provider store={store}>
+          <Main />
+        </Provider>
+      </BrowserRouter>
+    );
+    const input = screen.getByPlaceholderText(/Search/i);
+    expect((input as HTMLInputElement).value).toBe(localStorage.getItem('searchTerm'));
+  });
+});
