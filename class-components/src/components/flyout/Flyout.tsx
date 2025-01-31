f02499fffb0034dfc543a0630c9fc9e58b47c8c9
Kate <e.khizova@gmail.com>
2025-01-31 14:20:19 +0300

    feat: add close btn to flyout component

--- class-components/src/components/flyout/Flyout.tsx
+++ class-components/src/components/flyout/Flyout.tsx
@@ -3,9 +3,12 @@ type FlyoutProps = {
   selectedItems: number;
 };
 
+const onClose = () => {};
+
 export const Flyout = (props: FlyoutProps) => {
   return (
     <div className="flyout">
+      <img onClick={onClose} className="flyout__close" src="../../../assets/close-red.png" alt="close" />
       <button className="searchPanel__btn">Unselect all</button>
       <p>Total: {props.selectedItems}</p>
       <button className="searchPanel__btn" disabled>
