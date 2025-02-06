035281055ad6ffa64a99e96ab74252f3ecc6988f
Kate <e.khizova@gmail.com>
2025-02-06 13:14:53 +0300

    refactor: change name in header component test

--- class-components/src/components/header/header.test.tsx
+++ class-components/src/components/header/header.test.tsx
@@ -2,8 +2,8 @@ import { describe, expect, it } from 'vitest';
 import Header from './Header';
 import { render, screen } from '@testing-library/react';
 
-describe('App', () => {
-  it('renders header', () => {
+describe('Header', () => {
+  it('renders heading', () => {
     render(<Header />);
     expect(
       screen.getByRole('heading', {
