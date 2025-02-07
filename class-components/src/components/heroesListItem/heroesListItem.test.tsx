074f7ee2b12bef07e95a569c3c41f035698978b3
Kate <e.khizova@gmail.com>
2025-02-07 17:15:18 +0300

    test: add tests for card component

---
+++ class-components/src/components/heroesListItem/heroesListItem.test.tsx
@@ -0,0 +1,44 @@
+import { describe, expect, it, vi } from 'vitest';
+import { fireEvent, render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import { heroData } from '../../api/ApiConnector';
+import HeroesListItem from './heroesListItem';
+import Spinner from '../spinner/Spinner';
+
+const mockHeroData: heroData[] = [
+  {
+    id: 1,
+    name: 'mockHero1',
+    description: 'lorem',
+  },
+];
+const mockFn = vi.fn();
+
+describe('Test heroesListItem component', () => {
+  it('renders relevant card data', () => {
+    render(
+      <BrowserRouter>
+        <HeroesListItem key={mockHeroData[0].name} itemInfo={mockHeroData[0]} onCardClick={mockFn}></HeroesListItem>
+      </BrowserRouter>
+    );
+    expect(screen.getByText('mockHero1')).toBeInTheDocument();
+    expect(screen.getByText('lorem')).toBeInTheDocument();
+  });
+
+  it('opens a detailed card component when clicking on a card', () => {
+    const mockCardClickFn = vi.fn(() => render(<Spinner />));
+
+    render(
+      <BrowserRouter>
+        <HeroesListItem
+          key={mockHeroData[0].name}
+          itemInfo={mockHeroData[0]}
+          onCardClick={mockCardClickFn}
+        ></HeroesListItem>
+      </BrowserRouter>
+    );
+    fireEvent.click(screen.getByText('mockHero1'));
+    expect(mockCardClickFn).toHaveBeenCalledTimes(1);
+    expect(screen.getByRole('spinner')).toBeInTheDocument();
+  });
+});
