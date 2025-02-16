4be5fe78c02538b8adc9d39a91d175267328ffb9
Kate <e.khizova@gmail.com>
2025-02-16 17:22:48 +0300

    refactor: change css for dark theme

--- class-components/src/components/header/Header.tsx
+++ class-components/src/components/header/Header.tsx
@@ -1,8 +1,12 @@
+import { useContext } from 'react';
+import { ThemeContext } from '../../context/ThemeContext';
 import './header.css';
 
 const Header = () => {
+  const context = useContext(ThemeContext);
+
   return (
-    <header className="header">
+    <header className={`header header_${context.theme}`}>
       <h1 className="title">MARVEL HEROES</h1>
     </header>
   );
